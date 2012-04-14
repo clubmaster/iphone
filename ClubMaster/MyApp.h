346a5f8ceeec4a865be19db11534b4a6cb08b15f
Henrik Hansen <hh@fsck.dk>
2012-04-14 19:21:28 +0200

    Event functionality

--- ClubMaster/MyApp.h
+++ ClubMaster/MyApp.h
@@ -6,7 +6,14 @@
 //
 
 #define kLoginUrl @"%@/api/auth"
+
 #define kUserRegistrations @"%@/api/users/teams"
 #define kAllRegistrations @"%@/api/teams"
+
 #define kAttendTeam @"%@/api/teams/%@/attend"
 #define kUnattendTeam @"%@/api/teams/%@/unattend"
+
+#define kUserEvents @"%@/api/users/events"
+#define kAllEvents @"%@/api/events"
+#define kAttendEvent @"%@/api/events/%@/attend"
+#define kUnattendEvent @"%@/api/events/%@/unattend"
